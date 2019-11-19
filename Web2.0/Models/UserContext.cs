0a076377de1bef65c0d680591c257f19e45eb745
Axel M <Axel_mendez1@hotmail.com>
2019-11-19 00:08:49 -0300

    Proyecto ASP.NET

---
+++ Web2.0/Models/UserContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Web2._0.Models
+{
+    public partial class UserContext : DbContext
+    {
+        public UserContext()
+        {
+        }
+
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Test1;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.IdUser)
+                    .HasName("PK__USERS__B7C9263873F2A136");
+
+                entity.ToTable("USERS");
+
+                entity.Property(e => e.Contraseña)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Usuario)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
